bb2bf9c26ffdbdcd735600708703a587a009d034
miri-j-kim <144170686+miri-j-kim@users.noreply.github.com>
2023-11-27 18:03:29 -0700

    Update finalprojectquery.sql

--- SQL-scripts/finalprojectquery.sql
+++ SQL-scripts/finalprojectquery.sql
@@ -12,16 +12,16 @@ SELECT		Exhibit_name, Start_date, End_date
 FROM		EXHIBITION
 ORDER BY	Start_date DESC;
 
--- Q4: All art objects in 'The Tudors' exhibition
-SELECT	DISTINCT ART_OBJECT.Id_no, Title, Art_desc
-FROM	DISPLAYED_IN, ART_OBJECT
-WHERE	ART_OBJECT.Id_no IN (SELECT	Id_no
-							FROM	DISPLAYED_IN
-							WHERE	DISPLAYED_IN.Exhibit_name = 'The Tudors: Art and Majesty in Renaissance England');
+-- Q4: All art objects 'On loan'
+SELECT	DISTINCT Id_no, Title, Art_desc
+FROM	ART_OBJECT
+WHERE	Id_no IN 	(SELECT	Id_no
+					FROM	PERMANENT_COLLECTION
+					WHERE	Pcoll_status = 'On loan');
 
 -- Q5: Retrieve all artworks by Picasso
 SELECT	ART_OBJECT.Id_no, Title, Year_made, Style, Art_desc
-FROM	((ARTIST JOIN CREATES ON ARTIST.Artist_name = CREATES.Artist_name) JOIN ART_OBJECT ON CREATES.Id_no = ART_OBJECT.Id_no)
+FROM	ARTIST JOIN ART_OBJECT ON ARTIST.Artist_name = ART_OBJECT.Artist_name
 WHERE	ARTIST.Artist_name = 'Pablo Picasso';
 
 -- Q6: UPDATES all art_object with Baroque Epoche to be included in 
@@ -29,12 +29,12 @@ WHERE	ARTIST.Artist_name = 'Pablo Picasso';
 -- TRIGGER checks to ensure that art_object in Exhibitions that have dates that overlap
 -- 'The Rediscovery of The Baroque Period' Exhibition date are not moved.
 
-SELECT	*
-FROM 	DISPLAYED_IN;
+SELECT	Id_no, Title, Exhibit_name
+FROM 	ART_OBJECT;
 
 DROP TRIGGER IF EXISTS DISPLAYED_EXHIBIT_UPDATE_VIOLATION;
 CREATE TRIGGER DISPLAYED_EXHIBIT_UPDATE_VIOLATION
-BEFORE UPDATE ON DISPLAYED_IN
+BEFORE UPDATE ON ART_OBJECT
 FOR EACH ROW
 	SET NEW.Exhibit_name = IF(	(((SELECT DISTINCT End_date
 										FROM	EXHIBITION
@@ -52,22 +52,20 @@ FOR EACH ROW
 								OLD.Exhibit_name,
                                 NEW.Exhibit_name);
 
-UPDATE	DISPLAYED_IN
+UPDATE	ART_OBJECT
 SET		Exhibit_name = 'The Rediscovery of The Baroque Period'
-WHERE	Id_no IN	(SELECT	Id_no
-					FROM	ART_OBJECT
-                    WHERE	Epoch = 'Baroque');
+WHERE	Epoch = 'Baroque';
 
-SELECT	*
-FROM 	DISPLAYED_IN;
+SELECT	Id_no, Title, Exhibit_name
+FROM 	ART_OBJECT;
 
--- Q7: Deleted Objects from BORROWED collection move to PERMANENT_COLLECTION
-SELECT	*
+-- Q7: Delete artist tuple from database if not needed anymore
+SELECT	Artist_name, Artist_desc
 FROM ARTIST
 ORDER BY Artist_name;
-SELECT	*
-FROM CREATES
-ORDER BY Id_no;
+SELECT	Id_no, Title, Artist_name
+FROM ART_OBJECT
+ORDER BY Artist_name;
 
 DROP TRIGGER IF EXISTS ARTIST_NOT_NEEDED;
 CREATE TRIGGER ARTIST_NOT_NEEDED
@@ -75,13 +73,20 @@ BEFORE DELETE ON ART_OBJECT
 FOR EACH ROW
 	DELETE FROM	ARTIST
     WHERE Artist_name IN 	(SELECT		Artist_name
-							FROM		(ART_OBJECT JOIN CREATES ON ART_OBJECT.Id_no = CREATES.Id_no)
+							FROM		ART_OBJECT
                             WHERE		ART_OBJECT.Id_no = OLD.Id_no
                             GROUP BY	Artist_name
                             HAVING		COUNT(Artist_name) = 1);
 
 DELETE FROM	ART_OBJECT
 WHERE		Id_no = 'AO_0021' OR Id_no = 'AO_0022';
+
+SELECT	Artist_name, Artist_desc
+FROM ARTIST
+ORDER BY Artist_name;
+SELECT	Id_no, Title, Artist_name
+FROM ART_OBJECT
+ORDER BY Artist_name;
 
 SELECT	*
 FROM ARTIST
